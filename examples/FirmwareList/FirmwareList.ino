c6415f9641d6b94bfdb03a5d918a3cf21fad8d85
reaper7 <reaper7md@gmail.com>
2017-02-24 20:36:13 +0100

    new example FirmwareList

---
+++ examples/FirmwareList/FirmwareList.ino
@@ -0,0 +1,33 @@
+#include <ESP8266SPIFFSUpdateFirmware.h>
+
+SPIFFSUpdateFirmwareClass firmware;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("Start sketch");
+
+  Serial.print("SPIFFS init...");
+  if (firmware.begin()) {
+    Serial.println("OK");
+    Serial.print("Firmware file(s) count: ");
+    uint8_t filesnumber = firmware.getCount();
+    Serial.println(filesnumber);
+    if (filesnumber > 0) {
+      Serial.println("Firmware file(s) list:");
+      for (uint8_t i = 0; i < filesnumber; i++) {
+        Serial.print(" - ");
+        Serial.print(i);
+        Serial.print(" ;Size: ");
+        Serial.print(firmware.getSize(i));
+        Serial.print(" ;Name: ");
+        Serial.println(firmware.getName(i));
+      }
+    }     
+  } else
+    Serial.println("FAIL");  
+}
+
+void loop() {
+
+}
